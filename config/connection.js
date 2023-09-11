8b6fdefad9ba110e00d394f7f07c61bca28c2339
Yajiit <tjagerter@gmail.com>
2023-09-11 09:38:29 -0400

    added server and config files

---
+++ config/connection.js
@@ -0,0 +1,5 @@
+// config/connection.js
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmongoapi');
+
+module.exports = mongoose.connection;
